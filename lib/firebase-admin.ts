16d17e808d766a229502a361b6e7140c9e93bc74
Michael Ortiz <michael.ortiz.porrata@gmail.com>
2026-05-16 16:57:46 -0400

    Refactor Firebase private key handling in initFirebase function
    - Updated the logic to handle the private key by checking for escaped newline characters.
    - Ensured the private key is correctly formatted before being used in Firebase initialization.

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -10,7 +10,9 @@ function initFirebase() {
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.includes("\\n")
+            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+            : process.env.FIREBASE_PRIVATE_KEY,
       }),
     });
   } else {
